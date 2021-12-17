0755b31aceae941ccf00e0fd5e8a5dd294b54750
Haisheng Wu <haisheng.wu@okta.com>
2021-12-16 21:44:02 -0800

    init nodejs sample

---
+++ nodejs/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const { firstName, lastName } = req.query;
+  res.send(`Hello Mr. ${firstName} ${lastName}`);
+});
+
+module.exports = router;
