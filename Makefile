4c37d9792640c69d48c93751263270a80410c95a
Haisheng Wu <haisheng.wu@okta.com>
2021-12-16 23:13:14 -0800

    haskell sample

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 start_ngrok:
 	ngrok http 9001
 
-ngrok_server=a819-2601-648-8801-a0b0-2cf1-3fb8-1fcf-dee7
+ngrok_server=0a90-2601-648-8801-a0b0-2cf1-3fb8-1fcf-dee7
 
 test:
 	echo "GET http://$(ngrok_server).ngrok.io?first_name=aa&last_name=bb" | vegeta attack -rate 1000 -duration 10s | vegeta report
---
+++ haskell/app/Main.hs
@@ -0,0 +1,33 @@
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import           Data.List
+import           Data.Maybe
+import qualified Data.Text                     as T
+import           Data.Text
+import           GHC.Generics
+import           Network.Wai.Handler.Warp
+import           Servant
+
+type API
+  = QueryParam "first_name" Text :> QueryParam "last_name" Text :> Get '[PlainText] Text
+
+handlers :: Server API
+handlers = handleHome
+ where
+  handleHome :: Maybe Text -> Maybe Text -> Handler Text
+  handleHome fname lname =
+    return $ (T.pack "Hello, ") <> (fromMaybe "" fname) <> (fromMaybe "" lname)
+
+app1 :: Application
+app1 = serve (Proxy @API) handlers
+
+main :: IO ()
+main = run 9001 app1
