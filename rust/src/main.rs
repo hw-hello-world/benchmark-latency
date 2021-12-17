a28bafbc4443d7e1ab010632e0299b09bf82b6ec
Haisheng Wu <haisheng.wu@okta.com>
2021-12-17 09:22:54 -0800

    refactor rust server; update readme.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,10 @@
 #[macro_use] extern crate rocket;
 
 #[get("/?<first_name>&<last_name>")]
-fn index(first_name: &str, last_name: &str) -> &'static str {
-    "Hello Rocket!"
+fn index(first_name: &str, last_name: &str) -> String {
+    let result = format!("[Rust] Hello {} {}", first_name, last_name);
+    // &result[..]
+    result
 }
 
 #[launch]
